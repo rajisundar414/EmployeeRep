d79275b390b4c83e1f1de17c20ec9eb1537db665
Rajalakshmi <rajisundar414@gmail.com>
2022-07-10 21:48:38 +0530

    second commit

---
+++ Data/EmpdbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmpApp.Models;
+
+namespace EmpApp.Data
+{
+    public class EmpdbContext : DbContext
+    {
+        public EmpdbContext (DbContextOptions<EmpdbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmpApp.Models.Employee>? Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpApp.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+
+        public string EDesignation { get; set; }
+
+        public DateTime? EDOJ { get; set; }
+
+    }
+}
